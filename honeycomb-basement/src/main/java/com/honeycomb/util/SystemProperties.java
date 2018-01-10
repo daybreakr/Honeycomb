4a9330b808a128407823a3cf063fdf9e6ddd4544
ethan <ethanye@foxmail.com>
2018-01-10 12:33:59 +0800

    Optimize SystemProperties

--- honeycomb-basement/src/main/java/com/honeycomb/util/SystemProperties.java
+++ honeycomb-basement/src/main/java/com/honeycomb/util/SystemProperties.java
@@ -4,18 +4,32 @@ import android.annotation.SuppressLint;
 
 import java.lang.reflect.Method;
 
+// TODO: Hidden API, should be tested.
+
 public class SystemProperties {
+    private static Method sGetMethod;
 
     public static String get(String key) {
         try {
-            Method getMethod = getSystemPropertiesClass().getMethod("get", String.class);
+            Method getMethod = getSystemPropertiesGetMethod();
             return (String) getMethod.invoke(null, key);
         } catch (Exception e) {
             return null;
         }
     }
 
-    // TODO: Hidden API, should be tested.
+    @SuppressLint("PrivateApi")
+    private static Method getSystemPropertiesGetMethod() {
+        if (sGetMethod == null) {
+            try {
+                Class<?> clazz = getSystemPropertiesClass();
+                sGetMethod = clazz.getMethod("get", String.class);
+            } catch (Exception ignored) {
+            }
+        }
+        return sGetMethod;
+    }
+
     @SuppressLint("PrivateApi")
     private static Class<?> getSystemPropertiesClass() throws ClassNotFoundException {
         return Class.forName("android.os.SystemProperties");
