af4d5b0c0cdb6cee37a5190c50955558a96477f8
ethan <ethanye@foxmail.com>
2018-01-10 11:53:25 +0800

    Add some methods in Preconditions.

--- honeycomb-basement/src/main/java/com/honeycomb/util/Preconditions.java
+++ honeycomb-basement/src/main/java/com/honeycomb/util/Preconditions.java
@@ -2,6 +2,13 @@ package com.honeycomb.util;
 
 public class Preconditions {
 
+    public static int checkPositiveInteger(int integer, String errorMsg) {
+        if (integer <= 0) {
+            throw new IllegalArgumentException(errorMsg);
+        }
+        return integer;
+    }
+
     public static <T> T checkNotNull(final T reference) {
         if (reference == null) {
             throw new NullPointerException();
@@ -28,5 +35,25 @@ public class Preconditions {
             throw new IllegalArgumentException(String.valueOf(errorMessage));
         }
         return string;
+    }
+
+    public static void checkArgument(boolean expression) {
+        if (!expression) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Ensures that an expression checking an argument is true.
+     *
+     * @param expression the expression to check
+     * @param errorMessage the exception message to use if the check fails; will
+     *     be converted to a string using {@link String#valueOf(Object)}
+     * @throws IllegalArgumentException if {@code expression} is false
+     */
+    public static void checkArgument(boolean expression, final Object errorMessage) {
+        if (!expression) {
+            throw new IllegalArgumentException(String.valueOf(errorMessage));
+        }
     }
 }
