aff20bde2e51ec8ae23ddb34a3c03dcaf444b5d5
ethan <ethanye@foxmail.com>
2018-02-06 15:35:44 +0800

    Improve StringUtils.

--- honeycomb-basement/src/main/java/com/honeycomb/util/StringUtils.java
+++ honeycomb-basement/src/main/java/com/honeycomb/util/StringUtils.java
@@ -1,12 +1,70 @@
 package com.honeycomb.util;
 
-public class StringUtils {
+import com.honeycomb.log.HLog;
 
-    public static boolean isEmpty(String str) {
-        return str == null || str.trim().length() == 0;
+public class StringUtils {
+    private static final String EMPTY_STRING = "";
+
+    public static boolean isEmpty(CharSequence str) {
+        if (str == null || str.length() == 0) {
+            return true;
+        } else if (str instanceof String) {
+            return ((String) str).trim().length() == 0;
+        }
+        return false;
     }
 
-    public static boolean equals(String a, String b) {
-        return (a != null && a.equals(b)) || (a == null && b == null);
+    public static String nullIfEmpty(String str) {
+        return isEmpty(str) ? null : str;
+    }
+
+    public static String emptyIfNull(String str) {
+        return str == null ? EMPTY_STRING : str;
+    }
+
+    public static boolean equals(CharSequence a, CharSequence b) {
+        if (a == b) {
+            return true;
+        }
+        int length;
+        if (a != null && b != null && (length = a.length()) == b.length()) {
+            if (a instanceof String && b instanceof String) {
+                return a.equals(b);
+            } else {
+                for (int i = 0; i < length; i++) {
+                    if (a.charAt(i) != b.charAt(i)) return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void replaceAll(StringBuilder builder, String placeHolder, String replacement) {
+        if (isEmpty(placeHolder)) {
+            return;
+        }
+
+        replacement = emptyIfNull(replacement);
+
+        try {
+            final int holderLen = placeHolder.length();
+            final int replacementLen = replacement.length();
+            int start = 0;
+            while ((start = builder.indexOf(placeHolder, start)) >= 0) {
+                int end = start + holderLen;
+                if (end > builder.length()) {
+                    break;
+                }
+
+                builder.replace(start, end, replacement);
+                start += replacementLen;
+                if (start >= builder.length()) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            HLog.e(e);
+        }
     }
 }
