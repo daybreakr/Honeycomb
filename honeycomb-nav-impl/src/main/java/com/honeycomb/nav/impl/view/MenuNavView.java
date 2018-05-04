96e1b96a1206a9e4472b3f8b9eabf59fd34653a2
ethan <ethanye@foxmail.com>
2018-05-04 18:16:20 +0800

    Change NavigationViewNavView to MenuNavView to make a more general implementation.

--- honeycomb-nav-impl/src/main/java/com/honeycomb/nav/impl/view/MenuNavView.java
+++ honeycomb-nav-impl/src/main/java/com/honeycomb/nav/impl/view/MenuNavView.java
@@ -1,7 +1,5 @@
 package com.honeycomb.nav.impl.view;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.text.TextUtils;
 import android.util.SparseArray;
 import android.view.Menu;
@@ -12,27 +10,33 @@ import com.honeycomb.nav.NavItem;
 
 import java.util.List;
 
-public class NavigationViewNavView extends BaseNavView
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MenuNavView extends BaseNavView {
     private static final int CATEGORY_ORDER_WEIGHT = 100;
 
-    private NavigationView mNavigationView;
+    private Menu mMenu;
 
     private SparseArray<NavItem> mNavItems = new SparseArray<>();
 
-    public NavigationViewNavView(NavigationView navigationView) {
-        mNavigationView = navigationView;
-        mNavigationView.setNavigationItemSelectedListener(this);
+    public MenuNavView(Menu menu) {
+        mMenu = menu;
+    }
+
+    public boolean selectMenuItem(MenuItem menuItem) {
+        NavItem navItem = mNavItems.get(menuItem.getItemId());
+        if (navItem != null) {
+            invokeNavItemSelected(navItem);
+        }
+        return false;
     }
 
     @Override
     protected void onDetach() {
-        mNavigationView = null;
+        mMenu = null;
     }
 
     @Override
     public void bindNavItems(List<NavCategory> categories) {
-        if (mNavigationView == null) {
+        if (mMenu == null) {
             return;
         }
 
@@ -43,7 +47,7 @@ public class NavigationViewNavView extends BaseNavView
             NavCategory category = categories.get(categoryId);
 
             int categoryOrder = category.order * CATEGORY_ORDER_WEIGHT;
-            Menu categoryMenu = bindCategoryMenu(mNavigationView.getMenu(), category, categoryId,
+            Menu categoryMenu = bindCategoryMenu(mMenu, category, categoryId,
                     categoryOrder);
 
             for (int iIndex = 0, iCount = category.getItemsCount(); iIndex < iCount; iIndex++) {
@@ -74,14 +78,5 @@ public class NavigationViewNavView extends BaseNavView
         if (item.iconRes > 0) {
             menuItem.setIcon(item.iconRes);
         }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        NavItem navItem = mNavItems.get(menuItem.getItemId());
-        if (navItem != null) {
-            invokeNavItemSelected(navItem);
-        }
-        return false;
     }
 }
